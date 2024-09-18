c78fa15227b1ef677390f93156ce534a4298a869
Odogwu Chukwuebuka <71191684+odogwuEbuka@users.noreply.github.com>
2024-09-18 19:12:36 +0100

    updated base url

--- src/Utils/URLs/api.env.ts
+++ src/Utils/URLs/api.env.ts
@@ -1,4 +1,4 @@
-export const baseURL = "https://1325-102-212-210-56.ngrok-free.app";  //"https://visum-staging.buildbankng.com";    //"https://api.buildbankng.com"  //"https://visum-staging.buildbankng.com"
-export const buildAIURL = "https://1325-102-212-210-56.ngrok-free.app";  
+export const baseURL = "http://206.81.26.115:9090";  //"https://visum-staging.buildbankng.com";    //"https://api.buildbankng.com"  //"https://visum-staging.buildbankng.com"
+export const buildAIURL = "http://206.81.26.115:9090";  
 
 //Staging credentials ....email:brendanchudy9@gmail.com password: Password@1

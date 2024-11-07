faa9dfbbe4bdf95f72e3eee6ec831fd415cfb9c9
Fatima Modupe Bello <mailtomodupe@gmail.com>
2024-11-07 17:03:54 +0100

    components

--- src/pages/InventorySystem/ManageStore/Index.tsx
+++ src/pages/InventorySystem/ManageStore/Index.tsx
@@ -1,14 +1,24 @@
+
+import { useEffect, useState } from "react";
 import InventoryDashboardLayout from "../../../components/inventory/layout/Index";
+import Loading from "../../../components/Loading";
+import Cards from "../../../components/inventory/components/cards";
 
 
 const InventoryManageStore = () => {
+  const [loading, setLoading] = useState(true);
+ 
   return (
     <InventoryDashboardLayout>
-      <p className="text-[40px] font-bold text-[#177196]">Manage Store</p>
-
-  
+      <div className="grid bg-[#fff]  gap-[20px] pl-[40px] pr-[80px] py-[20px]">  
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        </div>
     </InventoryDashboardLayout>
   );
 };
+
+
 
 export default InventoryManageStore;
